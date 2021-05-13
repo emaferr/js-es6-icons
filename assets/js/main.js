15951acbd3f02a0a52eb2d8a3b3fe4ca2d5fd14d
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-05-13 16:48:20 +0200

    Completata prima Millestone

---
+++ assets/js/main.js
@@ -0,0 +1,121 @@
+// Milestone 1:
+
+// Partendo dalla seguente struttura dati 
+
+const icone = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	}
+];
+
+// Mostriamo in pagina tutte le icone disponibili come da layout.
+
+icone.forEach((icona) => {
+
+    document.getElementById("contenitore_card").insertAdjacentHTML("beforeend" , 
+    `
+    <div class="card my-5 mx-3 p-3 text-center shadow-sm" style="width: 14rem;">
+        <i class="${icona.family} ${icona.prefix}${icona.name}"></i>
+        <div class="">
+            <span style="font-size: .8rem;" class="card-text text-uppercase">${icona.name}</span>
+        </div>
+     </div>
+    `
+    )
+
+}) ;
+
+// console.log(icone);
