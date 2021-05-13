23b1599206a2b53c0880174ff5486c1149640f24
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-05-13 17:09:50 +0200

    Completata Milestone 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -105,12 +105,22 @@ const icone = [
 
 icone.forEach((icona) => {
 
+    // Milestone 2 Coloriamo le icone per tipo
+    
+    if (icona.type === "animal") {
+        icona.colore = "blue" 
+    }else if (icona.type === "user") {
+        icona.colore = "blueviolet" 
+    }else {
+        icona.colore = "orange"
+    }
+    
     document.getElementById("contenitore_card").insertAdjacentHTML("beforeend" , 
     `
-    <div class="card my-5 mx-3 p-3 text-center shadow-sm" style="width: 14rem;">
-        <i class="${icona.family} ${icona.prefix}${icona.name}"></i>
+    <div class="card my-5 mx-3 py-4 text-center shadow-sm" style="width: 14rem;">
+        <i style="color: ${icona.colore};" class="${icona.family} ${icona.prefix}${icona.name}"></i>
         <div class="">
-            <span style="font-size: .8rem;" class="card-text text-uppercase">${icona.name}</span>
+            <span style="font-size: .8rem ;" class="card-text text-uppercase font-weight-bold">${icona.name}</span>
         </div>
      </div>
     `
@@ -118,4 +128,5 @@ icone.forEach((icona) => {
 
 }) ;
 
-// console.log(icone);
+
+
