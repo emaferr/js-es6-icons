c87c09dccad1158b9c441af19002ada8c020872e
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-05-14 01:40:14 +0200

    Ordinato codice

--- assets/js/main.js
+++ assets/js/main.js
@@ -101,77 +101,36 @@ var icone = [
 	}
 ];
 
-// Mostriamo in pagina tutte le icone disponibili come da layout.
+// mostriamo in pagina tutte le icone disponibili come da layout.
 
-// var markup =  
-// `
-// <div class="card my-5 mx-3 py-4 text-center shadow-sm" style="width: 14rem;">
-//     <i style="color: ${icona.colore};" class="${icona.family} ${icona.prefix}${icona.name}"></i>
-//     <div class="">
-//         <span style="font-size: .8rem ;" class="card-text text-uppercase font-weight-bold">${icona.name}</span>
-//     </div>
-//  </div>
-// ` ;
+icone.forEach((icona) => {
 
+    // Milestone 2 Coloriamo le icone per tipo
 
+    if (icona.type === "animal") {
+        icona.colore = "blue" 
+    }else if (icona.type === "user") {
+        icona.colore = "blueviolet" 
+    }else {
+        icona.colore = "orange"
+    }
+
+    var markup = 
+        `
+        <div class="card mb-5 mt-4 mx-4 py-4 px-5 text-center shadow-sm" style="width: 15rem;">
+            <i style="color: ${icona.colore};" class="${icona.family} ${icona.prefix}${icona.name}"></i>
+            <div class="">
+                <span style="font-size: .8rem ;" class="card-text text-uppercase font-weight-bold">${icona.name}</span>
+            </div>
+        </div>
+        `
+        ;
+
+        document.getElementById("contenitore_card").insertAdjacentHTML("beforeend" , markup)
+
+})
 
 // Milestone 3 Creiamo una select con i tipi di icone e usiamola per filtrare le icone
-
-// var selezione = document.getElementById("exampleFormControlSelect1") ;
-
-// selezione.addEventListener("change" , function() {
-
-//         document.getElementById("contenitore_card").innerHTML = "" ;
-
-//         icone.forEach((icona) => {
-
-//             // Milestone 2 Coloriamo le icone per tipo
-        
-//             if (selezione.value === "animal") {
-//                 icona.colore = "blue"
-
-//                 document.getElementById("contenitore_card").insertAdjacentHTML("beforeend" , 
-//             `
-//             <div class="card my-5 mx-3 py-4 text-center shadow-sm" style="width: 14rem;">
-//                 <i style="color: ${icona.colore};" class="${icona.family} ${icona.prefix}${icona.name}"></i>
-//                 <div class="">
-//                     <span style="font-size: .8rem ;" class="card-text text-uppercase font-weight-bold">${icona.name}</span>
-//                 </div>
-//              </div>
-//             `
-//             )
-//             }else if (selezione.value === "vegetable") {
-//                 icona.colore = "orange"
-
-//                 document.getElementById("contenitore_card").insertAdjacentHTML("beforeend" , 
-//             `
-//             <div class="card my-5 mx-3 py-4 text-center shadow-sm" style="width: 14rem;">
-//                 <i style="color: ${icona.colore};" class="${icona.family} ${icona.prefix}${icona.name}"></i>
-//                 <div class="">
-//                     <span style="font-size: .8rem ;" class="card-text text-uppercase font-weight-bold">${icona.name}</span>
-//                 </div>
-//              </div>
-//             `
-//             )
-//             }else if (selezione.value === "user") {
-//                 icona.colore = "purple"
-
-//                 document.getElementById("contenitore_card").insertAdjacentHTML("beforeend" , 
-//             `
-//             <div class="card my-5 mx-3 py-4 text-center shadow-sm" style="width: 14rem;">
-//                 <i style="color: ${icona.colore};" class="${icona.family} ${icona.prefix}${icona.name}"></i>
-//                 <div class="">
-//                     <span style="font-size: .8rem ;" class="card-text text-uppercase font-weight-bold">${icona.name}</span>
-//                 </div>
-//              </div>
-//             `
-//             )
-//             } 
-
-//         }) ;
-
-// })
-
 
 var selezione = document.getElementById("exampleFormControlSelect1") ;
 
@@ -180,16 +139,6 @@ selezione.addEventListener("change" , function () {
     document.getElementById("contenitore_card").innerHTML=""
 
     icone.forEach((icona) => {
-
-        // Milestone 2 Coloriamo le icone per tipo
-
-        if (icona.type === "animal") {
-            icona.colore = "blue" 
-        }else if (icona.type === "user") {
-            icona.colore = "blueviolet" 
-        }else {
-            icona.colore = "orange"
-        }
 
         var markup = 
         `
